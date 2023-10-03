fef7f25252afa62ea64017f1e6a857cb64ab393f
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-03 12:59:28 -0300

    Async concepts

---
+++ Assincronidade/index.js
@@ -0,0 +1,12 @@
+function step02 () {
+    console.log('Passo 02')
+}
+
+setTimeout(() => {
+    step02()
+}, 1)
+
+console.log('Passo 01')
+console.log('Passo 03')
+console.log('Passo 04')
+console.log('Passo 05')
