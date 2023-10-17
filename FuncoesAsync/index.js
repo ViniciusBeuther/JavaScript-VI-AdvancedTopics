634327b15259935374f94587be07107acd9d17f5
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-17 12:53:07 -0300

    Async functions

---
+++ FuncoesAsync/index.js
@@ -0,0 +1,32 @@
+// Async Functions
+/*
+async function asyncSum(a, b) {
+    return a + b
+}
+
+async function asyncSubtract(a, b) {
+    return a - b
+}
+
+const sumResult = asyncSum(50, 33)
+const subResult = asyncSubtract(50, 33)
+
+Promise.all([ sumResult, subResult ]).then(results => {
+    console.log(results)
+}).catch(err => {
+    console.log(err)
+})
+
+*/
+
+const numbers = [4, 9, 5, 13, 77]
+
+async function asyncSquare(x) {
+    return x * x
+}
+
+Promise.all(numbers.map(number => asyncSquare(number))).then(squares => {
+    console.log(squares)
+}).catch((err) => {
+    console.log(err)
+})
