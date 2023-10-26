7c4ffff332e553956bfe0287ceab2a66742da30c
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-25 23:35:43 -0300

    Trying to update the balance

--- ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
+++ ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
@@ -1,7 +1,7 @@
+
 // Render transactions in screen
 function renderTransaction(transactionData) {
     const transactionTable = document.querySelector('#transaction-area');
-    
     const tableRow = document.createElement('tr');
     const tableCategory = document.createElement('td');
     const tableAmount = document.createElement('td');
@@ -29,9 +29,12 @@ function renderTransaction(transactionData) {
     if (transactionData.type === 'Expense'){
         tableAmount.classList.add('text-dark-expense')
         tableAmount.textContent = `- \$${amountFloat.toFixed(2)}`;
+        transactionList.push(amountFloat * -1)
+
     } else if (transactionData.type === 'Earning'){
         tableAmount.classList.add('text-success')
         tableAmount.textContent = `+ \$${amountFloat.toFixed(2)}`;
+        transactionList.push(amountFloat)
     } else {
         throw new Error (`Invalid transaction type = ${transactionData.type}`)
     }
@@ -41,36 +44,41 @@ function renderTransaction(transactionData) {
 
 }
 
+// Load the transactions from data base when the page refresh
+document.addEventListener("DOMContentLoaded", () => {
+    fetchTransactions()
+})
+
 // Get API data
 async function fetchTransactions(){
     const transactions = await fetch("http://localhost:3000/transaction").then(res => res.json())
     transactions.forEach(renderTransaction)
 }
 
-// Load the transactions from data base when the page refresh
-document.addEventListener("DOMContentLoaded", () => {
-    fetchTransactions()
-})
 
 // Get HTML elements FORM, Expense button and Earning button
-const form = document.querySelector('#form-section')
-const submitExpense = document.querySelector('#expense-button')
-const submitEarning = document.querySelector('#earning-button')
+const form = document.querySelector('#form-section');
+const submitExpense = document.querySelector('#expense-button');
+const submitEarning = document.querySelector('#earning-button');
+const transactionList = []
+
+console.log(transactionList)
+
 
 // Check which button call the Submit
 let transactionType = ''
 submitExpense.addEventListener('click', () => {
-    transactionType = 'Expense'
+    transactionType = 'Expense';
 })
 
 submitEarning.addEventListener('click', () => {
-    transactionType = 'Earning'
+    transactionType = 'Earning';
 })
 
 // Reset form and get input values, POST in Json Data Base
 form.addEventListener('submit', async (ev) => {
     ev.preventDefault();
-
+    updateBalance()
     const transactionData = {
         category: document.querySelector('#category').value,
         amount: document.querySelector('#amount-input').value,
@@ -91,3 +99,28 @@ form.addEventListener('submit', async (ev) => {
     form.reset();
     
 });
+
+
+// ============================================= // 
+
+// Update Balance 
+
+// ============================================= //
+
+async function updateBalance() {
+    let total = 0.00;
+    const balanceField = document.querySelector('#balance-field');
+    transactionList.forEach((transaction) => {
+        if(transaction > 0){
+            console.log(transaction);
+            total += transaction;
+        } else{
+            console.log(transaction);
+            total -= transaction;
+        }
+        
+    })
+
+    balanceField.placeholder = `$ ${total.toFixed(2).toString()}`
+    console.log(total)
+}
