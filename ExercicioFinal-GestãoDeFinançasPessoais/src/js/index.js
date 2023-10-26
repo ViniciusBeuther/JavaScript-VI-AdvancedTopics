7ff1adbfe7cf8fc2d5f96040bc2ac978adc73e94
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-25 21:22:44 -0300

    Made API work
    Fixed API GET/POST method.
    Now the website do:
    1. Get the values of inputs.
    2. Register in database.json using POST.
    3. Update the transaction list using GET.
    4. If submit using expense button the text will register with "-" in front of amout and paint it red. If using the earning, will add "+" and paint it green.
    5. Upgrade in style.

--- ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
+++ ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
@@ -1,3 +1,4 @@
+// Render transactions in screen
 function renderTransaction(transactionData) {
     const transactionTable = document.querySelector('#transaction-area');
     
@@ -7,52 +8,86 @@ function renderTransaction(transactionData) {
     const tableDate = document.createElement('td');
     const deleteButton = document.createElement('button')
 
-    tableRow.classList.add('table-row', 'mb-3')
+    tableRow.classList.add('table-row', 'mb-1')
     tableCategory.classList.add('table-data');
     tableAmount.classList.add('table-data');
     tableDate.classList.add('table-data');
     deleteButton.classList.add('remove-line-btn')
 
+    const amountFloat = parseFloat(transactionData.amount)
+    const formattedDate = transactionData.date.replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/,"$3\/$2\/$1")
+    console.log(typeof transactionData.category)
+    const mixedCategory = transactionData.category[0].toUpperCase() + transactionData.category.slice(1);
+
     tableRow.id = `transaction-${transactionData.id}`;
-    tableCategory.textContent = `${transactionData.category}`;
-    tableAmount.textContent = `\$${transactionData.amount.toFixed(2)}`;
-    tableDate.textContent = `${transactionData.date}`;
+    tableCategory.textContent = `${mixedCategory}`;
+    tableDate.textContent = `${formattedDate}`;
     deleteButton.textContent = `X`
     deleteButton.id = `delete-button-${transactionData.id}`
+    tableRow.classList.add('bg-light-font')
+// Checking type, if expense render red and if earning render green
+    if (transactionData.type === 'Expense'){
+        tableAmount.classList.add('text-dark-expense')
+        tableAmount.textContent = `- \$${amountFloat.toFixed(2)}`;
+    } else if (transactionData.type === 'Earning'){
+        tableAmount.classList.add('text-success')
+        tableAmount.textContent = `+ \$${amountFloat.toFixed(2)}`;
+    } else {
+        throw new Error (`Invalid transaction type = ${transactionData.type}`)
+    }
 
     tableRow.append(tableCategory, tableAmount, tableDate, deleteButton)
     transactionTable.appendChild(tableRow)
 
 }
 
+// Get API data
 async function fetchTransactions(){
     const transactions = await fetch("http://localhost:3000/transaction").then(res => res.json())
     transactions.forEach(renderTransaction)
 }
 
+// Load the transactions from data base when the page refresh
 document.addEventListener("DOMContentLoaded", () => {
     fetchTransactions()
 })
 
+// Get HTML elements FORM, Expense button and Earning button
 const form = document.querySelector('#form-section')
-form.addEventListener('Submit',async (ev) => {
-    ev.preventDefault()
+const submitExpense = document.querySelector('#expense-button')
+const submitEarning = document.querySelector('#earning-button')
+
+// Check which button call the Submit
+let transactionType = ''
+submitExpense.addEventListener('click', () => {
+    transactionType = 'Expense'
+})
+
+submitEarning.addEventListener('click', () => {
+    transactionType = 'Earning'
+})
+
+// Reset form and get input values, POST in Json Data Base
+form.addEventListener('submit', async (ev) => {
+    ev.preventDefault();
 
     const transactionData = {
-        category: document.getElementById('#category').value,
-        amount: document.getElementById('#amount-input').value,
-        date: document.getElementById('#date-input').value
+        category: document.querySelector('#category').value,
+        amount: document.querySelector('#amount-input').value,
+        date: document.querySelector('#date-input').value,
+        type: transactionType
     }
 
-    const response = await fetch("https://localhost:3000/transaction", {
+    const response = await fetch("http://localhost:3000/transaction", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(transactionData)
-    })
+    });
 
-    const savedTransaction = await response.json()
-    form.reset()
-    renderTransaction(savedTransaction)
-})
+    const savedTransaction = await response.json();
+    renderTransaction(savedTransaction);
+    form.reset();
+    
+});
