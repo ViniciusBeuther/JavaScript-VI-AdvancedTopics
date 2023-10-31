4cc52bf2ba95f7cf5e7d47a5c214155902095a95
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-30 23:38:29 -0300

    Adjusting

--- ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
+++ ExercicioFinal-GestãoDeFinançasPessoais/src/js/index.js
@@ -1,114 +1,83 @@
-<<<<<<< HEAD
-import { RenderTransaction } from "./RenderTransactions.js";
+let total = 0;
+let transactionType = '';
+const form = document.querySelector('#form-section');
+const submitExpenseBtn = document.querySelector('#expense-button');
+const submitEarningBtn = document.querySelector('#earning-button');
+const balanceInput = document.querySelector('#balance-field');
 
-=======
-function renderTransaction(transactionData) {
+
+function RenderTransaction(transactionData) {
     const transactionTable = document.querySelector('#transaction-area');
-    
     const tableRow = document.createElement('tr');
     const tableCategory = document.createElement('td');
     const tableAmount = document.createElement('td');
     const tableDate = document.createElement('td');
-    const deleteButton = document.createElement('button')
+    const deleteButton = document.createElement('button');
 
-    tableRow.classList.add('table-row', 'mb-3')
+    tableRow.classList.add('table-row', 'mb-1');
     tableCategory.classList.add('table-data');
     tableAmount.classList.add('table-data');
     tableDate.classList.add('table-data');
-    deleteButton.classList.add('remove-line-btn')
+    deleteButton.classList.add('remove-line-btn', 'mr-1');
+
+    const amountFloat = parseFloat(transactionData.amount);
+    const formattedDate = transactionData.date.replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})/, "$3/$2/$1");
+    const mixedCategory = transactionData.category[0].toUpperCase() + transactionData.category.slice(1);
 
     tableRow.id = `transaction-${transactionData.id}`;
-    tableCategory.textContent = `${transactionData.category}`;
-    tableAmount.textContent = `\$${transactionData.amount.toFixed(2)}`;
-    tableDate.textContent = `${transactionData.date}`;
-    deleteButton.textContent = `X`
-    deleteButton.id = `delete-button-${transactionData.id}`
+    tableCategory.textContent = `${mixedCategory}`;
+    tableAmount.textContent = `${amountFloat.toFixed(2)}`;
+    tableDate.textContent = `${formattedDate}`;
+    deleteButton.textContent = `X`;
+    deleteButton.id = `delete-button-${transactionData.id}`;
+    tableRow.classList.add('bg-light-font');
 
-    tableRow.append(tableCategory, tableAmount, tableDate, deleteButton)
-    transactionTable.appendChild(tableRow)
-
+    tableRow.append(tableCategory, tableAmount, tableDate, deleteButton);
+    transactionTable.appendChild(tableRow);
 }
 
-async function fetchTransactions(){
+async function fetchTransaction() {
     const transactions = await fetch("http://localhost:3000/transaction").then(res => res.json())
-    transactions.forEach(renderTransaction)
-}
->>>>>>> parent of 7ff1adb (Made API work)
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetchTransactions()
-})
-
-<<<<<<< HEAD
-// Get API data
-async function fetchTransactions(){
-    const transactions = await fetch("http://localhost:3000/transaction").then(res => res.json())
-    transactions.forEach(RenderTransaction)
+    transactions.forEach(RenderTransaction);
 }
 
-
-// Get HTML elements FORM, Expense button and Earning button
-const form = document.querySelector('#form-section');
-const submitExpense = document.querySelector('#expense-button');
-const submitEarning = document.querySelector('#earning-button');
-const transactionList = []
-
-console.log(transactionList)
-
-
-// Check which button call the Submit
-let transactionType = ''
-submitExpense.addEventListener('click', () => {
-    transactionType = 'Expense';
+document.addEventListener('DOMContentLoaded', () => {
+    fetchTransaction();
 })
 
-submitEarning.addEventListener('click', () => {
-    transactionType = 'Earning';
-})
-
-// Reset form and get input values, POST in Json Data Base
-/*
 form.addEventListener('submit', async (ev) => {
     ev.preventDefault();
-    UpdateBalance()
-=======
-const form = document.querySelector('#form-section')
-form.addEventListener('Submit',async (ev) => {
-    ev.preventDefault()
+    
+    const amountInput = document.querySelector('#amount-input');
+    const amount = parseFloat(amountInput.value); // Converta o valor para um número
 
->>>>>>> parent of 7ff1adb (Made API work)
+    balanceInput.placeholder = total.toFixed(2); // Use placeholder em vez de placeHolder
+
+    submitEarningBtn.addEventListener('click', () => {
+        transactionType = 'Earning';
+        console.log('entrou saldo')
+    }) 
+    submitExpenseBtn.addEventListener('click', () => {
+        transactionType = 'Expense';
+        console.log('entrou gasto')
+    })
+
     const transactionData = {
-        category: document.getElementById('#category').value,
-        amount: document.getElementById('#amount-input').value,
-        date: document.getElementById('#date-input').value
-    }
+        category: document.querySelector('#category').value,
+        amount: amount,
+        date: document.querySelector('#date-input').value,
+        type: transactionType,
+    };
 
-    const response = await fetch("https://localhost:3000/transaction", {
+    const response = await fetch("http://localhost:3000/transaction", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(transactionData)
-    })
+    });
 
-<<<<<<< HEAD
     const savedTransaction = await response.json();
-    renderTransaction(savedTransaction);
+    RenderTransaction(savedTransaction);
     form.reset();
-    
 });
-
-*/
-// ============================================= // 
-
-// Update Balance 
-
-// ============================================= //
-
-
-=======
-    const savedTransaction = await response.json()
-    form.reset()
-    renderTransaction(savedTransaction)
-})
->>>>>>> parent of 7ff1adb (Made API work)
